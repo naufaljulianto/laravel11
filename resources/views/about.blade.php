251fdbe5902a8a86459dc2673b0dbf211991f2e8
Muh. Naufal Julianto <novalja1235@gmail.com>
2024-10-30 19:53:56 +0700

    First Commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,7 @@
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot:title>
+    <h1>Halaman About</h1>
+    <h3>Owner {{ $nama }}</h3>
+    <img scr="img/foto.jpeg" alt="Foto Naufal">
+  </x-layout>
+    
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,8 @@
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot:title>
+    <ul>
+      <li><a href="https://instagram.com">Instagram</a>
+      <li><a href="https://x.com">X</a>
+      <li><a href="https://youtube.com">Youtube</a>
+    </ul>
+</x-layout>
